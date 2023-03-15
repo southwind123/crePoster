c99394ad27d6d25763771cbc0d4fd6b94dc4ad2e
zmk <2643395731@qq.com>
2023-03-15 13:27:38 +0800

    完成了 tabBar 的开发

---
+++ pages/poster/poster.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    poster
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
